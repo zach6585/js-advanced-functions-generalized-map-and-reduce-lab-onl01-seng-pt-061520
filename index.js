0c85acba7e1784288204b02f80b851de1e93f96b
zach6585 <zmines@wesleyan.edu>
2021-01-17 20:30:24 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Add your functions here
+function map(sourceArray, funaction) {
+    return sourceArray.map(funaction)
+}
+
+function reduce(sourceArray, funaction, startingValue) {
+    if (startingValue) {
+        return sourceArray.reduce(funaction, startingValue)
+    }
+    else {
+        return sourceArray.reduce(funaction)
+    }
+}   
+   
+
+
+
+
+
+
